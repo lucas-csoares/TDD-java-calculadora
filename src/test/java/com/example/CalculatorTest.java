7657890ac4725121050f53fb7912aadf2944d1ab
Lucas Soares <polonio230@hotmail.com>
2023-08-28 22:25:17 -0300

    Configured .gitignore to ignore Gradle files and added CalculatorTest and Calculator classes

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package test.java.com.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(2, 3);
+        assertEquals(5, result);
+    }
+}
